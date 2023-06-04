084895dc3d4f107e1a05ccbea8be8af8bee210ac
choyeseol <choyeseol07@gmail.com>
2023-06-04 16:42:18 +0900

    star pyramid

---
+++ star pyramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, j, k;
+
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n-i; j++)
+            printf(" ");
+        for(k=0; k<i*2+1; k++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
