3db6b49fcf8aa254a0e28f6fc050261a99906c29
choyeseol <choyeseol07@gmail.com>
2023-06-05 12:49:26 +0900

    sign converter

---
+++ sign converter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float n;
+    
+    printf("Please enter the number you want to convert. :");
+    scanf("%f", &n);
+    
+    printf("%g", -n);
+    
+    return 0;
+}
